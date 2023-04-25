3caffe7b19b7fd7097fb348f9a8c75abe9aebaa6
dtoolz <donaldaudriel@gmail.com>
2023-04-25 14:41:03 +0100

    Add project files.

---
+++ GlassStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GlassStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
