0ae90aaf091e36fcf04f3eac3cde0a91be467b78
dtoolz <donaldaudriel@gmail.com>
2023-05-05 18:08:43 +0100

    creating categories and inserting into db

--- GlassStore/Models/Category.cs
+++ GlassStore/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace GlassStore.Models
 {
@@ -8,7 +9,9 @@ namespace GlassStore.Models
         public int Id { get; set; }
 
         [Required]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
