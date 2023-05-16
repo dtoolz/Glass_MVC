a8829d664bdb862b4067f01d329dabe2f78e382b
dtoolz <donaldaudriel@gmail.com>
2023-05-16 17:27:11 +0100

    delete category added

--- GlassStore/Controllers/CategoryController.cs
+++ GlassStore/Controllers/CategoryController.cs
@@ -69,5 +69,33 @@ namespace GlassStore.Controllers
             return View();
 
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteCategory(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
